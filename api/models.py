d7cbebc6b5ee97081e147a6370e75c6c2a820914
Dave Caulton <dcaulton@gmail.com>
2016-02-08 20:56:06 -0600

    Finish first pass at specifying schemas

--- api/models.py
+++ api/models.py
@@ -23,19 +23,59 @@ class SingletonModel(models.Model):
 
 class Settings(SingletonModel):
     id = models.UUIDField(default=uuid.uuid4, primary_key=True)
-    current_group = models.ForeignKey(Group)
+    current_group = models.ForeignKey('Group')
 
     class Meta:
         db_table = 'settings'
+
+
+class Sample(models.Model):
+    SPECTROMETER = 'spectro'
+    COLOR = 'color'
+    FLUORESCENCE = 'fluoro'
+    READING_TYPE_CHOICES = (
+        (SPECTROMETER, 'Spectrometer'),
+        (COLOR, 'Color'),
+        (FLUORESCENCE, 'Fluorescence'),
+    )
+
+
+    PHYSICAL = 'Physical' #taken from this spectrometer
+    VIRTUAL = 'Virtual' #estimated from physical data of an untested compound
+    DERIVED = 'Defined' #created from merging samples
+    RECORD_TYPE_CHOICES = (
+        (PHYSICAL, 'Physical'),
+        (VIRTUAL, 'Virtual'),
+        (DERIVED, 'Derived'),
+    )
+
+    FIND_MATCH = 'find_match'
+    CHECK_LIMITS = 'check_limits'
+    NONE = 'none'
+    POST_CAPTURE_PROCESSING_CHOICES = (
+        (FIND_MATCH, 'Search for a match'),
+        (CHECK_LIMITS, 'Compare to upper and lower limits'),
+        (NONE, 'No post capture processing'),
+    )
+
+    group = models.ForeignKey('Group')
+    reading_type = models.CharField(max_length=32, choices=READING_TYPE_CHOICES)
+    record_type = models.CharField(max_length=32, choices=RECORD_TYPE_CHOICES)
+    description = models.CharField(max_length=4096)
+    subject = models.ForeignKey('Subject')
+    average_magnitude = models.IntegerField(default=0)
+    representative_sample = models.ForeignKey('Sample')
+
+    class Meta:
+        db_table = 'sample'
 
 
 class Group(models.Model):
     id = models.UUIDField(default=uuid.uuid4, primary_key=True)
     name = models.CharField(max_length=1024)
     created = AutoCreatedField()
-    parent_group = models.ForeignKey(Group)
-    calibration = models.ForeignKey(Calibration)
-    subject = models.ForeignKey(Subject)
+    parent_group = models.ForeignKey('Group')
+    subject = models.ForeignKey('Subject')
     reading_type = models.CharField(max_length=32,
                                     choices=Sample.READING_TYPE_CHOICES,
                                     default=Sample.SPECTROMETER)
@@ -49,31 +89,84 @@ class Group(models.Model):
     class Meta:
         db_table = 'group'
 
-class Sample(models.Model):
-    SPECTROMETER = 'spectro'
-    COLOR = 'color'
-    FLUORESCENCE = 'fluoro'
-    READING_TYPE_CHOICES = (
-        (SPECTROMETER, 'Spectrometer'),
-        (COLOR, 'Color'),
-        (FLUORESCENCE, 'Fluorescence'),
-    )
 
-    FIND_MATCH = 'find_match'
-    CHECK_LIMITS = 'check_limits'
-    NONE = 'none'
-    POST_CAPTURE_PROCESSING_CHOICES = (
-        (FIND_MATCH, 'Search for a match'),
-        (CHECK_LIMITS, 'Compare to upper and lower limits'),
-        (NONE, 'No post capture processing'),
+class SampleData(models.Model):
+    sample = models.ForeignKey('Sample')
+    delta = models.ForeignKey('SampleDelta')
+    frequency = models.IntegerField()
+    magnitude = models.IntegerField()
+
+    class Meta:
+        db_table = 'sample_data'
+
+
+class SampleFeature(models.Model):
+    PEAK = 'peak'
+    VALLEY = 'valley'
+    SPIKE = 'spike'
+    HOLE = 'hole'
+    PLATEAU_START = 'plateau_start'
+    PLATEAU_END = 'plateau_end'
+    FEATURE_TYPE_CHOICES = (
+        (PEAK, 'Peak'),
+        (VALLEY, 'Valley'),
+        (SPIKE, 'Spike'),
+        (HOLE, 'Hole'),
+        (PLATEAU_START,'Start of Plateau'),
+        (PLATEAU_END, 'End of Plateau'),
     )
+    
+    sample = models.ForeignKey('Sample')
+    feature_type = models.CharField(max_length=32, choices=FEATURE_TYPE_CHOICES)
+    sharpness = models.IntegerField()
+    magnitude = models.IntegerField()
+
+    class Meta:
+        db_table = 'sample_feature'
+
+
+class SampleMatch(models.Model):
+    source_sample = models.ForeignKey('Sample', related_name = 'samplematch_source_sample')
+    reference_sample = models.ForeignKey('Sample', related_name = 'samplematch_reference_sample')
+    delta = models.ForeignKey('SampleDelta')
+    rating = models.FloatField(default=0.0)
+
+    class Meta:
+        db_table = 'sample_match'
+
+
+class Photo(models.Model):
+    sample = models.ForeignKey('Sample')
+    file_path = models.CharField(max_length=1024)
+
+    class Meta:
+        db_table = 'photo'
+
+
+class VoiceMemo(models.Model):
+    sample = models.ForeignKey('Sample')
+    file_path = models.CharField(max_length=1024)
+
+    class Meta:
+        db_table = 'voice_memo'
+
+
+class SampleDelta(models.Model):
+    group = models.ForeignKey('Group')
+    source_sample = models.ForeignKey('Sample', related_name = 'sampledelta_source_sample')
+    reference_sample = models.ForeignKey('Sample', related_name = 'sampledelta_reference_sample')
+
+    class Meta:
+        db_table = 'sample_delta'
+
 
 class GroupMatchCandidate(models.Model):
-    group_id_of_sample = models.ForeignKey(Group)
-    group_id_of_reference_sample = models.ForeignKey(Group)
+    group_id_of_sample = models.ForeignKey('Group', related_name = 'source_sample_group')
+    group_id_of_reference_sample = models.ForeignKey('Group', related_name = 'reference_sample_group')
 
     class Meta:
         db_table = 'group_match_candidate'
+
 
 class GroupMember(models.Model):
     ACCESS_WEBAPP = 'access_webapp'
@@ -86,6 +179,50 @@ class GroupMember(models.Model):
         (TAKE_SAMPLES, 'Take samples'),
         (CONFIGURE_GROUP, 'Configure group'),
     )
-    group = models.ForeignKey(Group)
-    owner = models.ForeignKey(User)
-    role = models.CharField(max_length=32, Choices=ROLE_CHOICES)
+    group = models.ForeignKey('Group')
+    username = models.CharField(max_length=64)   #make this a foreign key to the Django admin User table
+    role = models.CharField(max_length=32, choices=ROLE_CHOICES)
+
+    class Meta:
+        db_table = 'group_member'
+
+
+class GroupLimit(models.Model):
+    SEND_EMAIL = 'send_email'
+    TAKE_ANOTHER_READING = 'take_another_reading'
+    NONE = 'none'
+    LIMITS_EXCEEDED_ACTION_CHOICES = (
+        (SEND_EMAIL, 'Send email to group members'),
+        (TAKE_ANOTHER_READING, 'Take another reading'),
+        (NONE, 'take no action'),
+    )
+    group = models.ForeignKey('Group')
+    upper_limit = models.ForeignKey('Sample', related_name = 'upper_limit')
+    lower_limit = models.ForeignKey('Sample', related_name = 'lower_limit')
+    limits_exceeded_action = models.CharField(max_length=32, choices=LIMITS_EXCEEDED_ACTION_CHOICES)
+
+    class Meta:
+        db_table = 'group_limit'
+
+
+class Location(models.Model):
+    group = models.ForeignKey('Group')
+    sample = models.ForeignKey('Sample')
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    elevation = models.FloatField()
+    datetime = models.FloatField()
+    number_of_satellites = models.IntegerField()
+
+    class Meta:
+        db_table = 'location'
+
+
+class Subject(models.Model):
+    name = models.CharField(max_length=1024)
+    description = models.CharField(max_length=4096)
+
+    class Meta:
+        db_table = 'subject'
+
+
