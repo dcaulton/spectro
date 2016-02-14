b025a2488fca761b883ed3642b9d5d40ae23a449
Dave Caulton <dcaulton@gmail.com>
2016-02-13 18:23:37 -0600

    Make calibrate endpoint asynchronous.

--- api/views.py
+++ api/views.py
@@ -2,7 +2,7 @@ import functools
 import uuid
 
 from django.shortcuts import get_object_or_404
-from django_q.tasks import async, result
+from django_q.tasks import async, result, Chain
 from rest_framework import viewsets
 from rest_framework.decorators import detail_route, api_view
 from rest_framework.response import Response
@@ -116,42 +116,24 @@ def capture_sample(request):
     Capture a physical sample from the spectrometer according to the parameters specified in the current active group
       from (the settings record)
     Capture a photo as well if the group is configured to capture photos
-    Save both records to the db
+    Both capture tasks are run asynchronously, the sample_id, photo_id, and task ids for both captures are returned
     '''
-    #TODO call voice record, save a VoiceMemo record
-    #TODO call sample post processing logic
-    #TODO have take_photo also run asynchronously, have it accept photo_id
+
     group = get_current_group()
     sample_id = uuid.uuid4()
 
-    sample = take_spectrometer_sample(sample_id=sample_id,
-                                      group_id=group.id,
-                                      reading_type=group.reading_type)
+    take_spectrometer_sample_task_id = async(take_spectrometer_sample,
+                                             sample_id,
+                                             group.id,
+                                             group.reading_type)
+
+    composite_data = {'sample': {'id': sample_id, 'task_id': take_spectrometer_sample_task_id}}
 
     if group.use_photo:
-        photo = take_photo(group, sample_id)
+        photo_id = uuid.uuid4()
+        take_photo_task_id = async(take_photo, photo_id, group, sample_id)
+        composite_data['photo'] = {'id': photo_id, 'task_id': take_photo_task_id}
 
-    sample_serializer = SampleSerializer(sample)
-    photo_serializer = PhotoSerializer(photo)
-    composite_data = {'sample': sample_serializer.data,
-                      'photo': photo_serializer.data}
-    return Response(composite_data)
-
-@api_view()
-def capture_sample_async(request):
-    '''
-    Capture a physical sample from the spectrometer according to the parameters specified in the current active group
-      from (the settings record)
-    Capture a photo as well if the group is configured to capture photos
-    Save both records to the db
-    '''
-
-    group = get_current_group()
-    sample_id = uuid.uuid4()
-
-    take_spectrometer_sample_task_id = async(take_spectrometer_sample, sample_id, group.id, group.reading_type)
-
-    composite_data = {'sample_id': sample_id, 'task_id': take_spectrometer_sample_task_id}
     return Response(composite_data)
 
 @api_view()
@@ -165,15 +147,16 @@ def calibrate(request):
     group = get_current_group()
 
     if 'reference_sample_id' in request.query_params and request.query_params['reference_sample_id']:
-        reference_sample = get_object_or_404(Sample, id=request.query_params['reference_sample_id'])
-        source_sample = take_spectrometer_sample(group_id=group.id,
-                                                 reading_type=group.reading_type)
+        source_sample_id = uuid.uuid4()
+        delta_id = uuid.uuid4()
+        chain = Chain(cached=True)
+        chain.append(take_spectrometer_sample, source_sample_id, group.id, group.reading_type)
+        chain.append(create_sample_delta, delta_id, group.id, source_sample_id, request.query_params['reference_sample_id'])
+        chain.run()
+        composite_data = {'sample': {'id': source_sample_id},
+                          'sample_delta': {'id': delta_id}}
 
-        delta = create_sample_delta(group, source_sample, reference_sample)
-
-        sample_delta_serializer = SampleDeltaSerializer(delta)
-
-        return Response(sample_delta_serializer.data)
+        return Response(composite_data)
     else:  #TODO add a test for this condition
         err_message = 'An id must be specified for a reference sample'
         return Response(data=err_message, status=409)
@@ -220,12 +203,14 @@ def get_current_group():
     group = get_object_or_404(Group, id=settings.current_group_id)
     return group
 
-def create_sample_delta(group, source_sample, reference_sample):
+def create_sample_delta(delta_id, group_id, source_sample_id, reference_sample_id):
     '''
     Compare the data between two samples
     Return a SampleDelta object with what needs to be added to the source sample to get the reference sample
     '''
     diff_data = []
+    source_sample = Sample.objects.get(id=source_sample_id)
+    reference_sample = Sample.objects.get(id=reference_sample_id)
     reference_sample_array = reference_sample.data.split(',')
     source_sample_array = source_sample.data.split(',')
 
@@ -233,7 +218,8 @@ def create_sample_delta(group, source_sample, reference_sample):
         diff_data.append(int(reference_sample_array[index]) - int(source_val))
 
     diff_data_csv_string = int_list_to_csv_string(diff_data)
-    sample_delta = SampleDelta(group=group,
+    sample_delta = SampleDelta(id=delta_id,
+                               group_id=group_id,
                                source_sample=source_sample,
                                reference_sample=reference_sample,
                                data=diff_data_csv_string)
@@ -293,12 +279,11 @@ def int_list_to_csv_string(array_of_ints):
     '''
     return functools.reduce(lambda x, y: str(x)+','+str(y), array_of_ints)
 
-def take_photo(group, sample_id):
+def take_photo(photo_id, group, sample_id):
     '''
     Calls the Picam class to have the hardware take a picture.
     Returns a Picture object
     '''
-    photo_id = uuid.uuid4()
     camera = Picam()
     file_path = camera.take_still(str(photo_id)+'.jpg')
     photo = Photo(id=photo_id,
